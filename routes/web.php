735c0cb107b319621bc4529918289f1698f7851f
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-01-26 17:23:25 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Tutte le rotte pubbliche per cui non serve autenticazione
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Tutte le rotte 'private' che prevedono autenticazione
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('projects', ProjectController::class);
+});
+
+//Tutte le rotte relative all'autenticazione
+require __DIR__.'/auth.php';
